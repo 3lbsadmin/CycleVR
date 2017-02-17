fb24603b13d61b36b2480077ec5ef7142e2fb9f9
eddie carbin <eddie@3lbs.com>
2017-02-17 15:06:51 -0800

    Start of CycleVR Arduino UNO 101

---
+++ CycleVR_UNO_101/CycleVR_UNO_101.ino
@@ -0,0 +1,21 @@
+#include <BLEAttribute.h>
+#include <BLECentral.h>
+#include <BLECharacteristic.h>
+#include <BLECommon.h>
+#include <BLEDescriptor.h>
+#include <BLEPeripheral.h>
+#include <BLEService.h>
+#include <BLETypedCharacteristic.h>
+#include <BLETypedCharacteristics.h>
+#include <BLEUuid.h>
+#include <CurieBLE.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
